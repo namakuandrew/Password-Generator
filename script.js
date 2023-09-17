088cc769d2bf26d4ba0189d7ea0f5b4eea02960c
namakuandrew <thesky11.lay@gmail.com>
2023-09-17 12:35:18 +0700

    adding js

--- script.js
+++ script.js
@@ -14,3 +14,43 @@ const character = {
 };
 
 //fungsi untuk mengenerate passwordnya
+const generatePassword = () => {
+  let staticPassword = "";
+  let randomPassword = "";
+  let exludeDuplicate = false;
+  let passLength = lengthSlider.value;
+
+  //looping untuk ulangi setiap option
+  option.forEach((option) => {
+    if (option.checked) {
+      if (option.id !== "exc-dupilcate" && option.id !== "spaces") {
+        //menambahkan character to password static jika setiap option itu dipilih
+        staticPassword += characters[option.id];
+      } else if (option.id == "spaces") {
+        staticPassword += ` ${staticPassword} `;
+      } else {
+        // jika opsi "spaces" dipilih, maka variabel excludeDuplicate akan diaktifkan.
+        excludeDuplicate = true;
+      }
+    }
+  });
+  // generate password secara acak dengan looping for
+  for (let i = 0; i < passLength; i++) {
+    let randomChar =
+      staticPassword[Math.floor(Math.random() * staticPassword.length)];
+    if (exludeDuplicate) {
+      if (!randomPassword.includes(randomChar) || randomChar == " ") {
+        randomPassword += randomChar;
+      } else {
+        i--;
+      }
+    } else {
+      // add random character ke password
+      randomPassword += randomChar;
+    }
+  }
+  // atur nilai dari input password
+  passwordInput.value = randomPassword;
+};
+
+generateBtn.addEventListener("click", generatePassword);
